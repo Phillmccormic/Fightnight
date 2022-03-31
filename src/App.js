c89ec193cbc38203bbb4fefa66d275a36b10301e
Matt Watts <matt@yoketeam.com>
2022-03-30 19:40:48 -0700

    update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hey boi
+          Hey there
         </p>
         <a
           className="App-link"
